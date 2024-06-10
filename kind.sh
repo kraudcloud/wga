f39aa6985064ba2b7e51e8c041e96427df2234aa
Karitham <kar@karitham.dev>
2024-06-10 14:13:44 +0200

    remove auto secret generation to force the user to backup the secret

--- kind.sh
+++ kind.sh
@@ -3,10 +3,13 @@
 docker build . -t ghcr.io/kraudcloud/wga:latest
 kind load docker-image ghcr.io/kraudcloud/wga:latest
 
+kubectl create secret generic wga-secret --from-literal=privateKey=$(head -c 32 /dev/urandom | base64)
+
 helm uninstall wga
 helm install wga ./charts/wga \
     --set endpoint.image.pullPolicy=Never \
     --set endpoint.image.tag=latest \
+    --set endpoint.privateKeySecretName=wga-secret \
     --set endpoint.address=192.168.1.10 \
     --set endpoint.clientCIDR=192.168.1.0/24 \
     --set endpoint.allowedIPs=192.168.1.0/24 \
